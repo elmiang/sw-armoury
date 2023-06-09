20438a73dfa008f8c10917176354a1e1154433bc
eldermiang <61782036+eldermiang@users.noreply.github.com>
2023-06-09 19:57:52 +1000

    Extend modal display/hide functionality to StarshipModals

--- src/components/StarshipModal.js
+++ src/components/StarshipModal.js
@@ -2,16 +2,25 @@ import React from "react";
 import { Modal } from "react-bootstrap";
 
 const StarshipModal = (props) => {
+  const onOverlayClick = (event) => {
+    props.toggleModal();
+    event.stopPropagation();
+  }
+
+  const onModalClick = (event) => {
+    event.stopPropagation();
+  }
+
   return(
-    <div>
-      <Modal show={props.modalOpen}>
-      <Modal.Header>
-        <Modal.Title>{props.name}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        
-      </Modal.Body>
-    </Modal>
+    <div className="modal-overlay" onClick={onOverlayClick}>
+      <Modal show={props.modalOpen} onClick={onModalClick}>
+        <Modal.Header>
+          <Modal.Title>{props.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
